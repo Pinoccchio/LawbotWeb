671e1c155fdff2120a6b9a41270870d915265990
pinocchio <jenjiliv@gmail.com>
2025-09-01 03:44:43 +0800

    01/09/2025 at 3:44 am

--- src/components/mobile-app-section.tsx
+++ src/components/mobile-app-section.tsx
@@ -7,7 +7,7 @@ import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
 import { QRCodeComponent } from '@/components/ui/qr-code'
 
-const APK_DOWNLOAD_URL = 'https://drive.google.com/file/d/1ijMHmlB6yAI5QbPW_Ubsifjh4woyH3vr/view?usp=sharing'
+const APK_DOWNLOAD_URL = 'https://drive.google.com/drive/folders/1Y00fx3vwra1NBkP5E0UMWico6TXkGfMd?usp=sharing'
 
 interface MobileAppSectionProps {
   isDark: boolean
